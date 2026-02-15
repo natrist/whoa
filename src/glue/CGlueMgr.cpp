defa0116adfbc425b567cdc2ee5272753ed7281e
Tristan Cormier <cormiert2@outlook.com>
2026-02-14 21:14:58 -0500

    feat(glue): add CGlueMgr::RaceChange

--- src/glue/CGlueMgr.cpp
+++ src/glue/CGlueMgr.cpp
@@ -1003,6 +1003,34 @@ void CGlueMgr::RealmListDialogCancelled() {
     }
 }
 
+void CGlueMgr::RaceChange(uint64_t guid, const CHARACTER_CREATE_INFO* info) {
+    if (guid && info) {
+        if (*info->name) {
+            SetIdleState(IDLE_9);
+
+            auto text = FrameScript_GetText("RACE_CHANGE_IN_PROGRESS", -1, GENDER_NOT_APPLICABLE);
+
+            FrameScript_SignalEvent(OPEN_STATUS_DIALOG, "%s%s", "CANCEL", text);
+
+            CDataStore netMsg;
+            netMsg.Put(static_cast<uint32_t>(CMSG_CHAR_RACE_CHANGE));
+            netMsg.Put(guid);
+            netMsg.PutString(info->name);
+            netMsg.Put(info->sexID);
+            netMsg.Put(info->skinID);
+            netMsg.Put(info->hairColorID);
+            netMsg.Put(info->hairStyleID);
+            netMsg.Put(info->facialHairStyleID);
+            netMsg.Put(info->faceID);
+            netMsg.Put(info->raceID);
+
+            netMsg.Finalize();
+
+            ClientServices::Send(&netMsg);
+        }
+    }
+}
+
 void CGlueMgr::Resume() {
     // TODO
     // CGlueMgr::m_disconnectPending = 0;
